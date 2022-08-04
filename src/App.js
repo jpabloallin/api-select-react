555a3fc8b5d8c5c184a2f07b7c01ef413886f163
jpabloallin <jpac.647@gmail.com>
2022-08-04 16:38:46 -0500

    App with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import FirstMenu from './components/FirstMenu';
+import SecondMenu from './components/SecondMenu';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex justify-content-around">
+        <div>
+          <FirstMenu/>
+        </div>
+        <div>
+          <SecondMenu/>
+        </div>
+      </div>
     </div>
   );
 }
